dc4ccf5d56ff0e70875161c33e51f2c877f97af4
Joachim Van Herwegen <joachimvh@gmail.com>
2018-03-14 09:51:54 +0100

    Don't assume all strings starting with p are PREFIX

--- N3Lexer.js
+++ N3Lexer.js
@@ -48,9 +48,10 @@ N3Lexer.prototype._parse = function (state)
     while (!state.eof())
     {
         var c = state.firstChar();
-        statements.push(this._statement(state));
+        var statement = this._statement(state);
+        statements.push(statement);
         // PREFIX and BASE
-        if (c !== 'P' && c !== 'p' && c !== 'B' && c !== 'b') // TODO: should we check for newlines?
+        if ((statement.type !== 'Prefix' && statement.type !== 'Base') || c === '@') // TODO: should we check for newlines?
             state.move('.');
     }
     return { type: 'Document', val: statements };
@@ -60,7 +61,9 @@ N3Lexer.prototype._statement = function (state)
 {
     var c = state.firstChar();
     var result;
-    if (c === '@' || c ==='P' || c === 'p' || c === 'B' || c === 'b')
+    if (c === '@' ||
+        ((c ==='P' || c === 'p') && state.firstWord().toLowerCase() === 'prefix') ||
+        ((c ==='B' || c === 'b') && state.firstWord().toLowerCase() === 'base'))
     {
         var first = state.firstWord();
         if (first === '@forAll')
